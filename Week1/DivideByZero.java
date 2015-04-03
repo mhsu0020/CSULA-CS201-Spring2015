7693b5069b4531d775b72d93d54e84cd2a8a755f
Michael Hsu <pig0826usanj@gmail.com>
2015-04-02 22:34:13 -0700

    Additional Examples codes
    additional code for week 1

---
+++ Week1/DivideByZero.java
@@ -0,0 +1,7 @@
+public class DivideByZero {
+	public static void main(String[] args) {
+			//Cannot divide by zero, runtime error
+			System.out.println(1/0);
+			
+		}
+}
---
+++ Week1/EscapeSequence.java
@@ -0,0 +1,15 @@
+//Example Comment
+/*
+*block comments
+*/
+
+//Class name must match file name
+public class EscapeSequence {
+	public static void main(String[] args) {
+			System.out.println("Example of new line escape seq.");
+			System.out.println("Hello\nWorld");
+			System.out.println("Example of tab escape seq.");
+			System.out.println("Hello\tWorld");
+
+		}
+}
